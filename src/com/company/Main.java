9b03879f066e4b68cf6a96f0bc7329d98fae6f0a
GBTC441008UR <GBTC441008UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-02-14 16:56:02 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> alist=new ArrayList<String>();
+
+
+
+
+
+
+    }
+}
+
+class Item {
+    String name;
+    double price;
+    int quantity;
+    String taxable;
+}
